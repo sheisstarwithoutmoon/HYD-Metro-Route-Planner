3639772b2e8a2a9ffc20f3539e228cc7e8b01f6c
Vanya <iamvanyaawasthi@gmail.com>
2025-03-25 22:48:36 +0530

    Updation in main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include<time.h>
 #include<windows.h>
 using namespace std;
-const int V=58;
+const int V=58; //Currently function stations as number of nodes
 struct station_code
 {
     string name;
@@ -16,36 +16,28 @@ struct station_code
     string color;
 };
 struct station_code station[V];
-float graph[V][V];
+float djk_graph[V][V];
 
 struct node1
-    {
-        vector <float>p;
-    }P[V];
-string makecapital(string str)
 {
-    for(int i=0;i<str.length();i++)
-    {
-        if(str[i]>96&&str[i]<123)
-            str[i]=str[i]-32;
-    }
-    return str;
-}
+    vector<float> p;
+} P[V];
+
 void drawbox(int, int, int, int, int);
-void secondWindow();
+void screen();
 void clrscreen();
 void delay(unsigned int ms);
-void gotoxy(int x,int y);
+void postition_x_y(int x,int y);
 int timetaken(float dist);
-int money(float dist);
-void Details(int t);
+int fare(float dist);
+
 void Path(float dist,int e,int st,int inter);
 int minDistance(float dist[], bool sptSet[]);
-void dijkstra(float graph[V][V], int src,int targ);
+void dijkstra(float djk_graph[V][V], int src,int dest);
 void printSolution(float dist[], int n,int src,int temp);
 void take_input();
 void logo(int x, int y);
-void secondWindow();
+void screen();
 void UI();
 
 COORD coord;
@@ -55,13 +47,24 @@ void clrscreen()
     drawbox(1,0,117,29,0);
 }
 
+//To avoid station not found error
+string makecapital(string str) 
+{
+    for(int i=0;i<str.length();i++)
+    {
+        if(str[i]>96&&str[i]<123)
+            str[i]=str[i]-32;
+    }
+    return str;
+}
+
 void delay(unsigned int ms)
 {
     clock_t goal=ms+clock();
     while(goal>clock());
 }
 
-void gotoxy(int x,int y)
+void postition_x_y(int x,int y)
 {
   coord.X=x;
   coord.Y=y;
@@ -70,33 +73,33 @@ void gotoxy(int x,int y)
 
 void drawbox(int x1, int y1, int x2, int y2, int d)
 {
-    gotoxy(x1,y1);
+    postition_x_y(x1,y1);
     printf("+");
     delay(d);
-    gotoxy(x2,y2);
+    postition_x_y(x2,y2);
     printf("+");
     delay(d);
     int i;
     for(i=1;i<x2-x1;i++)
-    {   gotoxy(x1+i,y1);
+    {   postition_x_y(x1+i,y1);
         printf("-");
         printf("\a");
         delay(d);
-        gotoxy(x2-i,y2);
+        postition_x_y(x2-i,y2);
         printf("-");
         delay(d);
     }
-    gotoxy(x1,y2);
+    postition_x_y(x1,y2);
     printf("+");
-    gotoxy(x2,y1);
+    postition_x_y(x2,y1);
     printf("+");
     int j;
     for(j=1;j<y2-y1;j++)
         {
-            gotoxy(x2,y1+j);
+            postition_x_y(x2,y1+j);
             printf("|");
             delay(d);
-            gotoxy(x1,y2-j);
+            postition_x_y(x1,y2-j);
             printf("|");
             delay(d);
         }
@@ -108,7 +111,7 @@ int timetaken(float dist)
     return ceil(dist/speed);
 }
 
-int money(float dist)
+int fare(float dist)
 {
     if(dist<=2)
         return 10;
@@ -121,13 +124,9 @@ int money(float dist)
     else if(dist>21&&dist<=32)
         return 50;
     else
-        return 60;
+        return 60; //maximum between miyapur and nagole
 }
 
-void Details(int t)
-{
-   // cout<<"\nthe benchmark location present near "<<t<<"is: "<<details[t].near<<endl;
-}
 
 void Path(float d,int e,int st,int inter)
 {
@@ -135,7 +134,7 @@ void Path(float d,int e,int st,int inter)
     static float dist=0;
     dist+=d;
 
-    gotoxy(16,11);
+    postition_x_y(16,11);
     cout<<"THE SHORTEST PATH IS : ";
 
     static int pos_x=12;
@@ -152,10 +151,10 @@ void Path(float d,int e,int st,int inter)
     string str;
     string color;
     if(!inter){
-    gotoxy(44,13);
+    postition_x_y(44,13);
     cout<<"****** LOADING ******";
     delay(2000);
-    gotoxy(42,13);
+    postition_x_y(42,13);
     cout<<"****** ROUTE FOUND ******";
     }
     vector<int>::iterator i2=path.end();
@@ -167,22 +166,10 @@ void Path(float d,int e,int st,int inter)
         color=station[*i].color;
         if(color=="BLUE")
             system("color 09");
-        else if(color=="YELLOW")
-            system("color 06");
-        else if(color=="PINK")
-            system("color 0D");
         else if(color=="RED")
             system("color 04");
-        else if(color=="MAGENTA")
-            system("color 05");
-        else if(color=="VOILET")
-            system("color 01");
         else if(color=="GREEN")
             system("color 02");
-        else if(color=="AQUA")
-            system("color 03");
-        else if(color=="ORANGE")
-            system("color 0C");
 
         if(i!=i2)
         {
@@ -190,18 +177,18 @@ void Path(float d,int e,int st,int inter)
         {
             pos_x=8;
             pos_y++;
-            gotoxy(pos_x,pos_y);
+            postition_x_y(pos_x,pos_y);
             cout<<"{change from "<<station[*i2].color<<" to "<<station[*i].color<<"}";
             pos_x=12;
             pos_y++;
-            gotoxy(pos_x,pos_y);
+            postition_x_y(pos_x,pos_y);
         }
         i2--;
         }
         str=station[*i].name;
         if(pos_x+5+str.size()<106)
         {
-            gotoxy(pos_x,pos_y);
+            postition_x_y(pos_x,pos_y);
             cout<<" ->> "<<str;
             pos_x=pos_x+str.size()+5;
         }
@@ -209,7 +196,7 @@ void Path(float d,int e,int st,int inter)
         {
             pos_x=12;
             pos_y++;
-            gotoxy(pos_x,pos_y);
+            postition_x_y(pos_x,pos_y);
             cout<<" ->> "<<str;
             pos_x=pos_x+str.size()+5;
         }
@@ -218,7 +205,7 @@ void Path(float d,int e,int st,int inter)
     }
     if(!inter)
     {
-      gotoxy(12,++pos_y);
+      postition_x_y(12,++pos_y);
       cout<<"INTERMEDIATE STATION";
       pos_y++;
       pos_x=12;
@@ -226,60 +213,60 @@ void Path(float d,int e,int st,int inter)
 
     if(inter){
     delay(1000);
-    gotoxy(72,11);
+    postition_x_y(72,11);
     cout<<"PATH LENGTH IS :";
-    gotoxy(97,11);
+    postition_x_y(97,11);
     cout<<d<<" KM";
     delay(1000);
-    gotoxy(16,10);
+    postition_x_y(16,10);
     cout<<"AVERAGE TIME : ";
-    gotoxy(39,10);
+    postition_x_y(39,10);
     cout<<timetaken(d)<<" MIN";
     delay(1000);
-    gotoxy(72,10);
+    postition_x_y(72,10);
     cout<<"AVERAGE FARE : ";
-    gotoxy(94,10);
-    cout<<" Rs. "<<money(d);
+    postition_x_y(94,10);
+    cout<<" Rs. "<<fare(d);
     delay(1000);
-    gotoxy(72,12);
+    postition_x_y(72,12);
     cout<<"NO OF STATIONS : ";
-    gotoxy(92,12);
+    postition_x_y(92,12);
     cout<<n_of_stations-1;
     }
     else{
       delay(1000);
-      gotoxy(72,11);
+      postition_x_y(72,11);
       cout<<"PATH LENGTH IS :";
-      gotoxy(89,11);
+      postition_x_y(89,11);
       cout<<d<<" KM,";
       delay(1000);
-      gotoxy(16,10);
+      postition_x_y(16,10);
       cout<<"AVERAGE TIME : ";
-      gotoxy(32,10);
+      postition_x_y(32,10);
       cout<<timetaken(d)<<" MIN,";
       delay(1000);
-      gotoxy(72,10);
+      postition_x_y(72,10);
       cout<<"AVERAGE FARE : Rs. ";
-      gotoxy(91,10);
-      cout<<money(d)<<",";
+      postition_x_y(91,10);
+      cout<<fare(d)<<",";
       delay(1000);
-      gotoxy(72,12);
+      postition_x_y(72,12);
       cout<<"NO OF STATIONS : ";
-      gotoxy(89,12);
+      postition_x_y(89,12);
       cout<<n_of_stations-1<<",";
     }
 
     if(inter){
     delay(2500);
-    gotoxy(44,9);
+    postition_x_y(44,9);
     cout<<"WANT TO SEARCH AGAIN ?  ";
     string choice;
     pos_x=12; pos_y=15;
     cin>>choice;
     choice=makecapital(choice);
     if(choice=="Y"||choice=="YES")
-        secondWindow();
-    gotoxy(5,30);
+        screen();
+    postition_x_y(5,30);
     char ch;
     scanf("%c",&ch);
     cout<<endl;
@@ -299,56 +286,58 @@ int minDistance(float dist[], bool sptSet[])
    return min_index;
 }
 
-void printSolution(float dist[], int n,int src,int temp,int inter)
+void printPath(float dist[], int n,int src,int temp,int inter)
 {
-    Details(temp);
+    // Details(temp);
     Path(dist[temp],temp,src,inter);
 }
 
-void dijkstra(float graph[V][V], int src,int targ,int inter)
+void dijkstra_algo(float djk_graph[V][V], int src, int dest, int inter)
 {
      float dist[V];
      bool sptSet[V];
-     for (int i = 0; i < V; i++)
+     for (int i = 0; i < V; i++){
         dist[i] = INT_MAX, sptSet[i] = false;
+     }
      dist[src] = 0;
      for (int count = 0; count < V-1; count++)
      {
-         int u = minDistance(dist, sptSet);
-       sptSet[u] = true;
-       for (int v = 0; v < V; v++)
-         if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
-                                       && dist[u]+graph[u][v] < dist[v])
+        int u = minDistance(dist, sptSet);
+        sptSet[u] = true;
+        for (int v = 0; v < V; v++)
+        {
+            if (!sptSet[v] && djk_graph[u][v] && dist[u] != INT_MAX
+                                       && dist[u]+djk_graph[u][v] < dist[v])
             {
-                dist[v] = dist[u] + graph[u][v];
+                dist[v] = dist[u] + djk_graph[u][v];
                 P[v].p.push_back(u);
             }
+        }
      }
-     printSolution(dist, V,src,targ, inter);
+     printPath(dist, V,src,dest, inter);
 }
 int lcs( string X,string Y ) 
 { 
 	int m=X.length();
 	int n=Y.length();
-   int L[m+1][n+1]; 
-   int i, j; 
+    int L[m+1][n+1]; 
+    int i, j; 
    
-   for (i=0; i<=m; i++) 
-   { 
-     for (j=0; j<=n; j++) 
-     { 
-       if (i == 0 || j == 0) 
-         L[i][j] = 0; 
-   
-       else if (X[i-1] == Y[j-1]) 
-         L[i][j] = L[i-1][j-1] + 1; 
-   
-       else
-         L[i][j] = max(L[i-1][j], L[i][j-1]); 
-     } 
-   } 
-   return L[m][n]; 
-} 
+    for (i=0; i<=m; i++) 
+    { 
+        for (j=0; j<=n; j++) 
+        { 
+            if (i == 0 || j == 0) 
+                L[i][j] = 0; 
+            else if (X[i-1] == Y[j-1]) 
+                L[i][j] = L[i-1][j-1] + 1; 
+            else
+                L[i][j] = max(L[i-1][j], L[i][j-1]); 
+        } 
+    } 
+    return L[m][n]; 
+}
+
 int sameMatch(string s){
 	int max=0,maxi=-1;
 	for(int i=0;i<58;i++){
@@ -362,118 +351,117 @@ int sameMatch(string s){
 		return -1;
 	return maxi;
 }
+
 void take_input()
 {		
-		char ch;
-		//scanf("%c",&ch);
-        string start_s,end_s,inter_s,line,col;
-        gotoxy(16,3);
-        cout<<"ENTER THE STARTING STATION:";
-        gotoxy(20,5);
-        getline(cin,start_s);
-        gotoxy(72,3);
-        cout<<"ENTER THE DESTINATION STATION:";
-        gotoxy(76,5);
-        getline(cin,end_s);
-        gotoxy(42,6);
-        cout<<"ENTER THE INTERMEDIATE STATION:";
-        gotoxy(46,8);
-        getline(cin,inter_s);
+    char ch;
+    //scanf("%c",&ch);
+    string start_s, end_s, inter_s, line, col;
+    postition_x_y(16,3);
+    cout<<"ENTER THE START STATION:";
+    postition_x_y(20,5);
+    getline(cin, start_s);
+    postition_x_y(42,6);
+    cout<<"ENTER THE INTERMEDIATE STATION:";
+    postition_x_y(46,8);
+    getline(cin,inter_s);
+    postition_x_y(72,3);
+    cout<<"ENTER THE DESTINATION STATION:";
+    postition_x_y(76,5);
+    getline(cin,end_s);
 
+    //for(int i=0;i<V;i++)
+    //cout<<station[i].name<<" "<<station[i].code<<" "<<station[i].color<<endl;
 
+    start_s = makecapital(start_s);
+    end_s = makecapital(end_s);
+    inter_s = makecapital(inter_s);
 
-        //for(int i=0;i<V;i++)
-        //cout<<station[i].name<<" "<<station[i].code<<" "<<station[i].color<<endl;
-
-        start_s=makecapital(start_s);
-        end_s=makecapital(end_s);
-        inter_s=makecapital(inter_s);
-
-        int startcode,endcode,intercode=-1;
-        int startflag=0,endflag=0,interflag1=0,interflag2=0;
-        if(!(inter_s=="NO"||inter_s=="N"))
-          interflag1=1;
-        startcode=sameMatch(start_s);
-        endcode=sameMatch(end_s);
-        intercode=sameMatch(inter_s);
-        if(startcode==-1)
-        	startflag=1;
-        if(endcode==-1)
-        	endflag=1;
-        if(endcode==-1)
-        	interflag2=1;
-        /*for(int i=0;i<58;i++)
+    int startcode, endcode, intercode=-1;
+    int startflag=0, endflag=0, interflag1=0, interflag2=0;
+    if(!(inter_s=="NO"||inter_s=="N"))
+        interflag1=1;
+    startcode = sameMatch(start_s);
+    endcode = sameMatch(end_s);
+    intercode = sameMatch(inter_s);
+    if(startcode==-1)
+        startflag=1;
+    if(endcode==-1)
+        endflag=1;
+    if(endcode==-1)
+        interflag2=1;
+    /*for(int i=0;i<58;i++)
+    {
+        if(station[i].name==start_s)
         {
-            if(station[i].name==start_s)
-            {
-                startcode=station[i].code;
-                startflag=1;
-            }
-            if(station[i].name==end_s)
-            {
-                endcode=station[i].code;
-                endflag=1;
-            }
-            if(interflag1&&station[i].name==inter_s)
-            {
-              intercode=station[i].code;
-              interflag2=1;
-            }
-        }*/
-        int fault=0;
-        if(startflag==1)
-        {
-            gotoxy(42,10);
-            cout<<"WRONG STARTING STATION NAME ENTERED";
-            fault=1;
+            startcode=station[i].code;
+            startflag=1;
         }
-        if(endflag==1)
+        if(station[i].name==end_s)
         {
-            gotoxy(40,11);
-            cout<<"WRONG DESTINATION STATION NAME ENTERED";
-            fault=1;
+            endcode=station[i].code;
+            endflag=1;
         }
-        if(interflag1&&interflag2==1)
+        if(interflag1&&station[i].name==inter_s)
         {
-            gotoxy(39,12);
-            cout<<"WRONG INTERMEDIATE STATION NAME ENTERED";
-            fault=1;
+            intercode=station[i].code;
+            interflag2=1;
         }
-        //ch;
-        scanf("%c",&ch);    
-        if(fault)
-        {
-          secondWindow();
-          return;
+    }*/
+    int fault=0;
+    if(startflag==1)
+    {
+        postition_x_y(42,10);
+        cout<<"WRONG STARTING STATION NAME ENTERED";
+        fault=1;
+    }
+    if(endflag==1)
+    {
+        postition_x_y(40,11);
+        cout<<"WRONG DESTINATION STATION NAME ENTERED";
+        fault=1;
+    }
+    if(interflag1&&interflag2==1)
+    {
+        postition_x_y(39,12);
+        cout<<"WRONG INTERMEDIATE STATION NAME ENTERED";
+        fault=1;
+    }
+    //ch;
+    scanf("%c",&ch);    
+    if(fault)
+    {
+        screen();
+        return;
+    }
+    if(interflag1){
+        dijkstra_algo(djk_graph,startcode,intercode,0);
+        dijkstra_algo(djk_graph,intercode,endcode,1);
         }
-        if(interflag1){
-          dijkstra(graph,startcode,intercode,0);
-          dijkstra(graph,intercode,endcode,1);
-          }
-        else
-          dijkstra(graph, startcode,endcode,1);
+    else
+        dijkstra_algo(djk_graph, startcode,endcode,1);
 }
 
 void logo(int x, int y)
 {
-    gotoxy(x,y);
-    printf("   ___                     ___       ____");
-    gotoxy(x,y+1);
-    printf(" ||   \\\\   ||\\\\    //||  ||   \\\\   //    \\\\");
-    gotoxy(x,y+2);
-    printf(" ||    ||  || \\\\  // ||  ||    || ||");
-    gotoxy(x,y+3);
-    printf(" ||    ||  ||  \\\\//  ||  ||___//  ||");
-    gotoxy(x,y+4);
-    printf(" ||    ||  ||        ||  ||  \\\\   ||");
-    gotoxy(x,y+5);
-    printf(" ||___//   ||        ||  ||   \\\\   \\\\____//");
-    }
+    postition_x_y(x,y);
+    printf("                                     ");
+    postition_x_y(x,y+1);
+    printf(" ||    ||  ||\\\\    //||  ||   \\\\ ");
+    postition_x_y(x,y+2);
+    printf(" ||    ||  || \\\\  // ||  ||    || ");
+    postition_x_y(x,y+3);
+    printf(" ||||||||  ||  \\\\//  ||  ||___// ");
+    postition_x_y(x,y+4);
+    printf(" ||    ||  ||        ||  ||  \\\\   ");
+    postition_x_y(x,y+5);
+    printf(" ||    ||  ||        ||  ||   \\\\  ");
+}
 
-void secondWindow()
+void screen()
 {
     clrscreen();
-    gotoxy(48,19);
+    postition_x_y(48,19);
     system("color 0C");
     delay(90);
     take_input();
@@ -496,17 +484,17 @@ void UI(){
     delay(90);
     system("color 0A");
     delay(90);
-    gotoxy(42,14);
-    cout<<"WELCOME TO DELHI METRO DESKTOP APP";
+    postition_x_y(42,14);
+    cout<<"WELCOME TO HYDERABAD METRO DESKTOP APP";
     system("color 0C");
     delay(90);
     system("color 0D");
     delay(90);
-    gotoxy(47,18);
+    postition_x_y(47,18);
     printf("PRESS ENTER TO CONTINUE");
     char ch;
     scanf("%c",&ch);
-    secondWindow();
+    screen();
 }
 
 int main()
@@ -515,47 +503,36 @@ int main()
     float dis;
     ifstream fin;
     fin.open("nodes.txt");
-/*     while (fin) {
-
-        // Read a Line from File
-        getline(fin, line);
-
-        // Print line in Console
-        cout << line << endl;
-    }
-*/
     for(int i=0;i<58;i++)
     {
        for(int j=0;j<58;j++)
-        graph[i][j]=0;
+        djk_graph[i][j]=0;
     }
     for(int i=1;i<=V;i++)
     {
         fin>>temp;
         fin>>n1;
-        //cout<<temp<<" "<<n1<<endl;
         for( int j=0;j<temp;j++)
         {
            fin>>n2;
            fin>>dis;
            //cout<<" "<<n2<<" "<<dis<<endl;
-           graph[n1-1][n2-1]=dis;
+           djk_graph[n1-1][n2-1]=dis;
         }
-        //cout<<endl;
     }
-        string line,col;
-        ifstream code;
-        ifstream color;
-        code.open("stationnames.txt");
-        color.open("colorcodes.txt");
-        for(int i=0;i<V;i++)
-        {
-            getline(code, line);
-            station[i].name=line;
-            station[i].code=i;
-            getline(color,col);
-            station[i].color=col;
-        }
+    string line,col;
+    ifstream code;
+    ifstream color;
+    code.open("stationnames.txt");
+    color.open("colorcodes.txt");
+    for(int i=0;i<V;i++)
+    {
+        getline(code, line);
+        station[i].name=line;
+        station[i].code=i;
+        getline(color,col);
+        station[i].color=col;
+    }
     //Writing the matrix
     ofstream matrix;
     matrix.open("matrix.txt");
@@ -569,13 +546,10 @@ int main()
         {
             if(j==0)
                 matrix<<i<<" ";
-            matrix<<graph[i][j]<<" ";
+            matrix<<djk_graph[i][j]<<" ";
         }
         matrix<<endl;
     }
     UI();
-    secondWindow();
-    //take_input(graph);
-    /*Uncomment UI for accessing the introductory slide 
-      uncomment secondWindow and comment UI for debugging from the input part*/
+    // screen();
 }
